58b419d4381ed62e9b6b68fa74d7d113e7f7de3e
vitorrrocha <vitorrochaflorip@hotmail.com>
2021-02-04 21:03:02 -0300

    Fix: Header, add Shape and Text link Nanny

---
+++ src/Components/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './style.css';
+
+class Header extends React.Component {
+    render() {
+        return(
+            <header>
+                <div className="shape">
+
+                    <div className="smile-mouth" >
+
+                    </div>
+                </div>
+                <div className="register-box">
+                    <h5><a href="#">Become a Nanny Share Host</a></h5>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
